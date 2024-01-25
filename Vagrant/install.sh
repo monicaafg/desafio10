c70a77c7106ac6f9ac414d4ec5ef7d870a27812c
monick <monicaafg@hotmail.com>
2024-01-25 00:09:59 -0300

    Creacion de VM

---
+++ Vagrant/install.sh
@@ -0,0 +1,73 @@
+# swapoff -a
+sudo usermod -a -G docker jenkins 
+sudo usermod -aG docker ${USER}
+
+# Instalar Java
+echo "Installing default-java"
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre -y
+
+# Instalar Jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+sleep 1m
+
+# Imprimir la contraseña inicial de Jenkins
+echo "Password Jenkins"
+JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+echo $JENKINSPWD
+
+# Instalar Docker
+echo "Installing docker"
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Install Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
+# Install Maven
+sudo apt install maven -y
+
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+apt-get update
+apt-get install -y vim curl wget git
+snap install microk8s --classic
+microk8s status --wait-ready
+microk8s enable registry storage dns ingress
+snap install kubectl --classic
+mkdir -p /root/.kube
+microk8s config > /root/.kube/config
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+ARGO_CLI_VERSION=$(curl --silent "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+curl -sSL -o /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/$ARGO_CLI_VERSION/argocd-linux-amd64
+chmod +x /usr/local/bin/argocd
+kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
+sleep 60
+echo "Password: $(sudo microk8s kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+#IP_ADDRESS=$(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+#echo "IP de Argocd: "$IP_ADDRESS
+fuser -k 8081/tcp
+#kubectl port-forward --address $IP_ADDRESS svc/argocd-server -n argocd 8081:443
+kubectl port-forward --address '192.168.33.13' svc/argocd-server -n argocd 8081:443
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
